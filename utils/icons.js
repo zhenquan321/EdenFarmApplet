53bfb5b9105f775c04b9bad1b66814de8acd6033
fengzhenquan <616447247@qq.com>
2018-12-09 20:35:52 +0800

    no message

--- utils/icons.js
+++ utils/icons.js
@@ -6,10 +6,15 @@ export default {
   paixu: '/static/images/paixu.png',
   loading: 'http://outf4pfsz.bkt.clouddn.com/loading.png',
   loading_back: 'http://outf4pfsz.bkt.clouddn.com/loading_back.png',
-  daifukuan:"/static/images/daifukuan.png",
+  daifukuan:"/static/images/userInfo/daifukuan.png",
   daipingjia: "/static/images/daipingjia.png",
-  daishouhuo: "/static/images/daishouhuo.png",
-  yiwancheng: "/static/images/yiwancheng.png",
-  map:"/static/images/map.png"
+  daifahuo: "/static/images/userInfo/daifahuo.png",
+  daishouhuo: "/static/images/userInfo/daisouhu.png",
+  yiwancheng: "/static/images/userInfo/yiwancheng.png",
+  member:"/static/images/userInfo/member.png",
+  diziguanli: "/static/images/userInfo/diziguanli.png",
+  phoneNum: "/static/images/userInfo/phoneNum.png",
 
+
+  
 }
